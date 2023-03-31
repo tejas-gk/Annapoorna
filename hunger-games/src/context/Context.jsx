bd2fbeb2f89da66cfb9cdae0e7a6b507f11cbab4
Anudeep K K <anudeep2003@gmail.com>
2023-03-31 18:55:47 +0530

    context

--- hunger-games/src/context/Context.jsx
+++ hunger-games/src/context/Context.jsx
@@ -5,16 +5,18 @@ import { cartReducer, productReducer } from "./Reducers";
 const Cart = createContext();
 faker.seed(99);
 
-const Context = ({ children }) => {
-  const products = [...Array(20)].map(() => ({
-    id: faker.datatype.uuid(),
-    name: faker.commerce.productName(),
-    price: faker.commerce.price(),
-    image: faker.random.image(),
-    inStock: faker.random.arrayElement([0, 3, 5, 6, 7]),
-    fastDelivery: faker.datatype.boolean(),
-    ratings: faker.random.arrayElement([1, 2, 3, 4, 5]),
-  }));
+  const Context = ({ children }) => {
+    const products = [...Array(20)].map(() => ({
+      id: faker.datatype.uuid(),
+      name: faker.commerce.productName(),
+      price: faker.random.number({ min: 5, max: 50, precision: 0.01 }),
+      image: faker.image.food(),
+      inStock: faker.random.arrayElement([0, 3, 5, 6, 7]),
+      fastDelivery: faker.datatype.boolean(),
+      ratings: faker.random.arrayElement([1, 2, 3, 4, 5]),
+    }));
+ 
+  
 
   const [state, dispatch] = useReducer(cartReducer, {
     products: products,
